bf2c40de2bdccaa58b911904e859b365854d56d8
Praveen Kumar Sharma <2022ac05656@wilp.bits-pilani.ac.in>
2024-08-03 04:25:24 +0530

    Expose sample application

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
-print('Hello, World!')
+from flask import Flask, jsonify
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define a route for the default URL, which serves as an endpoint
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return jsonify(message="Hello, World!")
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
