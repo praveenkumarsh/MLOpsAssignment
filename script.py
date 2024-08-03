9b2524291977ce89809a9680510fae0756b4d2c6
Praveen Kumar Sharma <2022ac05656@wilp.bits-pilani.ac.in>
2024-08-03 11:39:19 +0530

    Updated port

--- script.py
+++ script.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return jsonify(message="Hello, World!")
 
+@app.route('/', methods=['GET'])
+def hello_world_stash():
+    return jsonify(message="Hello, World! /")
+
+
 # Run the application
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
